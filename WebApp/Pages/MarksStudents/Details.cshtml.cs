963560e9f82a0d70bab55f0a538de95e47f4c30e
IviyanStoyanov <ivian.st1@gmail.com>
2023-03-22 00:17:37 +0200

    CSS for the two buttons and different UI depending if you are teacher or student

---
+++ WebApp/Pages/MarksStudents/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Data.Model;
+
+namespace WebApp.Pages.Marks
+{
+    public class DetailsModelStudent : PageModel
+    {
+        private readonly Data.Context _contextStudent;
+
+        public DetailsModelStudent(Data.Context context)
+        {
+            _contextStudent = context;
+        }
+
+      public Mark MarkStudents { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _contextStudent.Marks == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _contextStudent.Marks.FirstOrDefaultAsync(m => m.Id == id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                MarkStudents = mark;
+            }
+            return Page();
+        }
+    }
+}
---
+++ WebApp/Pages/MarksStudents/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using Data.Model;
+
+namespace WebApp.Pages.Marks
+{
+    public class DetailsModelStud : PageModel
+    {
+        private readonly Data.Context _contextStud;
+
+        public DetailsModelStud(Data.Context context)
+        {
+            _contextStud = context;
+        }
+
+        public IList<Mark> MarkStud { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_contextStud.Marks != null)
+            {
+                MarkStud = await _contextStud.Marks.ToListAsync();
+            }
+        }
+    }
+}
